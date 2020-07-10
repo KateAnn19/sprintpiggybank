b81968fbe459802256a172859dd0cb73e2b9cf97
Kate McGee <mcgeehee@gmail.com>
2020-07-10 09:39:22 -0600

    initial project setup

---
+++ src/main/java/local/kmcgeeka/newrepobank/NewrepobankApplication.java
@@ -0,0 +1,16 @@
+package local.kmcgeeka.newrepobank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewrepobankApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(NewrepobankApplication.class,
+            args);
+    }
+
+}
---
+++ src/test/java/local/kmcgeeka/newrepobank/NewrepobankApplicationTests.java
@@ -0,0 +1,15 @@
+package local.kmcgeeka.newrepobank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewrepobankApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
