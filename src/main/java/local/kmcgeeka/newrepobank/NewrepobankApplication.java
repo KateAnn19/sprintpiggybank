339ffd355b7273b9158446c6e79cbfdacf046e86
Kate McGee <mcgeehee@gmail.com>
2020-07-10 10:09:40 -0600

    endpoint working and messages printing correctly. need to adjust for plural words

--- src/main/java/local/kmcgeeka/newrepobank/NewrepobankApplication.java
+++ src/main/java/local/kmcgeeka/newrepobank/NewrepobankApplication.java
@@ -1,7 +1,11 @@
 package local.kmcgeeka.newrepobank;
 
+import local.kmcgeeka.newrepobank.models.Coin;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @SpringBootApplication
 public class NewrepobankApplication
@@ -9,8 +13,12 @@ public class NewrepobankApplication
 
     public static void main(String[] args)
     {
-        SpringApplication.run(NewrepobankApplication.class,
-            args);
+
+        SpringApplication.run(NewrepobankApplication.class, args);
+        System.out.println("Piggy Bank");
+        List<Coin> bankVault = new ArrayList<>();
+
+        System.out.println(bankVault.toString());
     }
 
 }
--- src/main/java/local/kmcgeeka/newrepobank/controllers/CoinController.java
+++ src/main/java/local/kmcgeeka/newrepobank/controllers/CoinController.java
@@ -1,5 +1,38 @@
 package local.kmcgeeka.newrepobank.controllers;
 
+import local.kmcgeeka.newrepobank.models.Coin;
+import local.kmcgeeka.newrepobank.repositories.CoinRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RestController
 public class CoinController
 {
+    @Autowired
+    CoinRepository coinrepos;
+    //Create an endpoint http://localhost:2019/total
+    // that prints to console the contents of the
+    // Piggy Bank as follows and returns an HTTP Status of OK:
+    @GetMapping(value = "/total", produces={"application/json"})
+    public ResponseEntity<?> bankTotal()
+    {
+        List<Coin> clist = new ArrayList<>();
+        coinrepos.findAll().iterator().forEachRemaining(clist::add);
+
+        double total = 0;
+        for(Coin c : clist)
+        {
+            total = total + (c.getValue() * c.getQuantity());
+            System.out.println(c.getQuantity() + " " + c.getName());
+        }
+
+        System.out.println("Total in the piggy bank is " + total);
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
